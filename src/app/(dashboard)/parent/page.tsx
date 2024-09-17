60dee80dd4eb1fdd0bc8dd7c7cb5049b75ff11ba
vsbron <vsbron@gmail.com>
2024-09-17 13:46:19 +0300

    Add the Dashboard components to the rest of the roles - Parent and Teacher

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,6 +1,23 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 function ParentPage() {
   // Returned JSX
-  return <div>Parent Page</div>;
+  return (
+    <div className=" flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-xl">
+          <h2 className="text-xl font-semibold">Schedule (John Doe)</h2>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
 }
 
 export default ParentPage;
