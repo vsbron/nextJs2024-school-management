48784d4a9c0d0f70cd8e575458d9909df0bc8696
vsbron <vsbron@gmail.com>
2024-09-17 14:47:25 +0300

    Add List page interface elements

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,34 @@
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+
+function TeacherList() {
+  return (
+    <div className="bg-white p-4 rounded-xl flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h2 className="hidden md:block text-lg font-semibold">All teachers</h2>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+              <Image src="/filter.png" width={14} height={14} alt="" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+              <Image src="/sort.png" width={14} height={14} alt="" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+              <Image src="/plus.png" width={14} height={14} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+}
+
+export default TeacherList;
---
+++ src/components/TableSearch.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+
+function TableSearch() {
+  // Returned JSX
+  return (
+    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+      <Image src="/search.png" alt="Search" width={14} height={14} />
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-[200px] p-2 bg-transparent outline-none"
+      />
+    </div>
+  );
+}
+
+export default TableSearch;
