7a2b3e7a4119626adc8025639b39e677531e8d1d
vsbron <vsbron@gmail.com>
2024-09-18 12:43:40 +0300

    Add subjects list similar to parents list

---
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -0,0 +1,91 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { role, subjectsData } from "@/lib/data";
+
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+
+type Subject = {
+  id: number;
+  name: string;
+  teachers: string[];
+};
+
+const columns = [
+  {
+    header: "Subject Names",
+    accessor: "subject",
+    className: "px-4",
+  },
+  {
+    header: "Teachers",
+    accessor: "teachers",
+    className: "hidden md:table-cell",
+  },
+];
+
+function TeacherList() {
+  const renderRow = (item: Subject) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-schoolPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <h3 className="font-semibold">{item.name}</h3>
+      </td>
+      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="rounded-full w-7 h-7 flex items-center justify-center bg-schoolSky">
+              <Image src="/view.png" width={16} height={16} alt="" />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="rounded-full w-7 h-7 flex items-center justify-center bg-schoolPurple">
+              <Image src="/delete.png" width={16} height={16} alt="" />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  // Returned JSX
+  return (
+    <div className="bg-white p-4 rounded-xl flex-1 m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h2 className="hidden md:block text-lg font-semibold">All teachers</h2>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+              <Image src="/filter.png" width={14} height={14} alt="" />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+              <Image src="/sort.png" width={14} height={14} alt="" />
+            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-schoolYellow">
+                <Image src="/plus.png" width={14} height={14} alt="" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table<Subject>
+        columns={columns}
+        renderRow={renderRow}
+        data={subjectsData}
+      />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+}
+
+export default TeacherList;
