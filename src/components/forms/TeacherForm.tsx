a839a010a37813063506e9ed4c4ee709bad94208
vsbron <vsbron@gmail.com>
2024-09-20 18:31:22 +0300

    Add React Hook Form package and basic template for Add or Edit data form

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { Teacher } from "@/lib/types";
+
+type TeacherFormInputs = {
+  example: string;
+  exampleRequired: string;
+};
+
+function TeacherForm({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: Teacher;
+}) {
+  // Getting the form functions from React Hook Form
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm<TeacherFormInputs>();
+
+  // Submit handler
+  const onSubmit: SubmitHandler<TeacherFormInputs> = (data) =>
+    console.log(data);
+
+  // Returned JSX
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="">
+      <input defaultValue="test" {...register("example")} />
+      <input {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default TeacherForm;
